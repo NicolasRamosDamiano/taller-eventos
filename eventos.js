2a27ed31a99bc9ecb834c6de2d2032aa6f37d26c
NicolasRamosDamiano <113316784+NicolasRamosDamiano@users.noreply.github.com>
2025-08-29 15:55:58 -0300

    arreglo
    arreglo

--- eventos.js
+++ eventos.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const contenedor = document.getElementById("contenedor");
+  const boton = document.getElementById("saludar");
 
   contenedor.addEventListener("click", () => {
     alert("Hola! Soy el div");
+  });
+
+  boton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert("Hola!");
   });
 });
