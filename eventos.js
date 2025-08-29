79b1a0b688ba77c40ee8506a0b070cb947f682be
NicolasRamosDamiano <113316784+NicolasRamosDamiano@users.noreply.github.com>
2025-08-29 15:38:55 -0300

    Agregar div con estilo y manejador de eventos

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const contenedor = document.getElementById("contenedor");
+
+  contenedor.addEventListener("click", () => {
+    alert("Hola! Soy el div");
+  });
+});
